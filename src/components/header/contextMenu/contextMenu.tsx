0f9ed127c59babd8bf1949116b771e909e93dac6
makson <myEmail.com>
2026-01-19 13:51:05 +0500

    поменял архиткутуру

--- src/components/header/contextMenu/contextMenu.tsx
+++ src/components/header/contextMenu/contextMenu.tsx
@@ -1,38 +1,44 @@
-import { useEffect, useRef } from "react"
+import { useRef, useState } from "react"
 import styles from "./contextMenu.module.css"
-interface props {
-    updateStateContextMenu: (state:boolean)=> void,
-    stateContextMenu: boolean,
-    tribar: HTMLElement,
-}
-function ContextMenu({updateStateContextMenu, stateContextMenu, tribar}:props) {
-    const contextMenuRef = useRef<HTMLUListElement>(null)
-    useEffect(()=>{
-        function handleOnClick(e: MouseEvent) {
-            if(e.target instanceof Node 
-                && !tribar.contains(e.target) 
-                && !contextMenuRef.current?.contains(e.target)) {
-                updateStateContextMenu(!stateContextMenu)
-            }
-        }
 
-        document.addEventListener('click', handleOnClick)
-        return () => {
-            document.removeEventListener('click', handleOnClick)
+function ContextMenu() {
+    const [stateContextMenu, setStateContextMenu] = useState<boolean>(false) 
+    const contextMenuRef = useRef<HTMLUListElement>(null)
+    const tribarRef = useRef<HTMLDivElement>(null)
+    function handleOnClickOutside(e: MouseEvent) {
+        if(e.target instanceof Node 
+            && tribarRef.current !== null
+            && !tribarRef.current.contains(e.target) 
+            && !contextMenuRef.current?.contains(e.target)
+        ) {
+            setStateContextMenu(false)  
         }
-    }) 
+    }
+    if(stateContextMenu) {
+        document.addEventListener('click', handleOnClickOutside)
+    }
+    else {
+        document.removeEventListener('click', handleOnClickOutside)
+    }
     return (
             <>
-            <ul className={styles.contextMenu} tabIndex={0} ref={contextMenuRef}>
-                <div>Фильтры:</div>
-                <li className={styles.beginNew}>Cначала новые</li>
-                <li className={styles.beginOld}>Cначала старые</li>
-                <li className={styles.beginName}>Сортировать по алфавиту</li>
-                <li className={styles.completeds}>Выполненные</li>
-                <li className={styles.uncompleteds}>Невыполненные</li>
-                <li className={styles.removeds}>Удалённые</li>
-            </ul>
-            <div className={styles.overlay}></div>
+                <div ref={tribarRef} className={styles.tribar} onClick={()=>setStateContextMenu((prev)=>!prev)}>
+                    ≡
+                </div>
+            {stateContextMenu && (
+            <>
+                <ul className={styles.contextMenu} tabIndex={0} ref={contextMenuRef}>
+                    <div>Фильтры:</div>
+                    <li className={styles.beginNew}>Cначала новые</li>
+                    <li className={styles.beginOld}>Cначала старые</li>
+                    <li className={styles.beginName}>Сортировать по алфавиту</li>
+                    <li className={styles.completeds}>Выполненные</li>
+                    <li className={styles.uncompleteds}>Невыполненные</li>
+                    <li className={styles.removeds}>Удалённые</li>
+                </ul>
+                <div className={styles.overlay}></div>
+            </>
+            )}
             </>
     )
 }
